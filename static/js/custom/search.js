da1ecdb01424141079cef14c33c5be4f2b4b40b3
heobu <esma.ozelbas@boun.edu.tr>
2020-12-27 11:08:45 +0300

    Cleaned up unnecessary comments.

--- static/js/custom/search.js
+++ static/js/custom/search.js
@@ -81,18 +81,12 @@ function removeDimension(dimensionId) {
 function requestSearch() {
     console.log("Search requested.");
 
+    // Extract dimensions from input and serialize.
     var dimensions_str = '{"dimensions":[';
     var dimensions = $("#dimensionsContainerId").children();
     for(var i=0; i < dimensions.length; i ++) {
         console.log(dimensions[i]);
         var id = dimensions[i].getAttribute("id");
-        console.log(id);
-        //console.log($("#" + id).find($(".bootstrap-tagsinput")));
-        /*var tags = $("#" + id).find($(".bootstrap-tagsinput").children());
-        console.log("num of tags: ", tags.length);
-        for(var j=0; j < tags.length; j ++) {
-            console.log(tags[j]);
-        }*/
         dimensions_str += '{"keywords": [';
         dimension_str = "";
         var tags = $("#" + id).find(".bootstrap-tagsinput").each(function() {
@@ -109,16 +103,12 @@ function requestSearch() {
         if(i + 1 < dimensions.length) {
             dimensions_str += ',';
         }
-        //console.log($("#" + id).find($(".tag .label .label-info")));
-        //dimensions[i].children(".dimension-form")[0];
     }
     dimensions_str += ']}';
     console.log("dim data: ", dimensions_str);
 
     var serializedData = {  // Data for POST request.
         main_query: $('#mainQueryId').val(),    // Main query
-        //dimension1: "lol",    // Dimension 1
-        //dimensions: '{"dimensions":[{"keywords": ["body", "image"]}, {"keywords": ["mri", "magnetic resonance imaging"]}]}'
         dimensions: dimensions_str
     };
     console.log("Post data: " + serializedData);
@@ -128,7 +118,6 @@ function requestSearch() {
         type: "post",
         data: serializedData,
         cache: 'false',
-        //async: 'true',
 
         success: function (response) {   // Successful at adding a relation.
             console.log("Search request is successful.");
