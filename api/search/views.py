222b7532d1ea5549e54bb834f27f2269a8e2d411
kerem <keremocakoglu@gmail.com>
2021-01-12 22:19:23 +0300

    performance problem solved with implemented dict logic while creating articles.

--- api/search/views.py
+++ api/search/views.py
@@ -193,10 +193,10 @@ class SearchHelper(object):
         if len(common_article_list) > 0:
             article_details_futures = []
             articles = []
-            with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
                 article_details_futures.append(executor.submit(self.get_article_details, common_article_list))
-            for x in as_completed(article_details_futures):
-                articles = x.result()
+                for x in as_completed(article_details_futures):
+                    articles = x.result()
             print("articles created for ", combination)
             if len(articles) > 0:
                 search_result = SearchResult(combination)
@@ -342,6 +342,16 @@ class SearchHelper(object):
                 article_id_list.append(target_id_str[len(target_id_str)-1])
         return article_id_list
 
+    #returns a dict that consist all articles in the mongodb
+    def retrieve_all_articles(self):
+        mongo_query = {}
+        document = self.annotation_detail_column.find(mongo_query)
+        all_papers = {}
+        for x in document:
+            list_item = dict(x)
+            all_papers[list_item["id"]]=list_item
+        return all_papers
+
     # takes article details from mongodb with its keyword
     def article_details_query(self, article_id):
         mongo_query = {}
@@ -366,12 +376,23 @@ class SearchHelper(object):
     # create collection of details of articles
     def get_article_details(self, article_list):
         articles = []
-        futures = []
-        while article_list:
-            with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-                futures.append(executor.submit(self.article_details_query, article_list.pop()))
-        for x in (futures):
-            articles.append(x.result())
+        all_articles = self.retrieve_all_articles()
+        # create all articles from given list
+        for article_id in article_list:
+            list_item=all_articles[article_id]
+            if article_id == list_item["id"]:
+                article = Article(pm_id=list_item["id"],
+                                  title=list_item["id"],
+                                  journal_issn="",
+                                  journal_name=list_item["journal_name"],
+                                  abstract="",
+                                  pubmed_link=list_item["pubmed_link"],
+                                  author_list=list_item["author_list"],
+                                  instutation_list=list_item["institution_list"],
+                                  article_date=list_item["article_date"],
+                                  top_three_keywords=list_item["top_three_keywords"],
+                                  article_type=list_item["article_type"])
+                articles.append(article)
         return articles
 
 
@@ -424,8 +445,6 @@ class Article(object):
         self.article_type = article_type
         self.json_val=self.toJSON()
 
-    #todo pm_id mi pm_link mi?
-    #TODO annotation_id den articles/id değiştiği için article_id veya annotation_id sunma gereği kalmadı
     def toJSON(self):
         return {
             "pm_id":self.pm_id,
@@ -529,7 +548,6 @@ class SearchResult(object):
     def generate_dict_value(self, response):
         dict = {}
         json_articles = []
-        # todo Document size too large with BSONObj size is invalid hatası geliyor ondan kapalı
         for article in self.articles:
             json_articles.append(article.json_val)
         dict["value"] = self.keyword.replace(")", " ")
