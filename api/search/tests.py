ea0844a09e9f510890194bb114b6dae540296999
heobu <esmaozelbas@gmail.com>
2021-01-31 22:59:33 +0300

    Added test and updated coverage report.

--- api/search/tests.py
+++ api/search/tests.py
@@ -2,7 +2,8 @@ import json
 
 from django.test import TestCase
 
-from api.search.views import SearchHelper
+from api.search.views import SearchHelper, Article
+
 
 class TestSearchMethods(TestCase):
     def setUp(self):
@@ -39,3 +40,24 @@ class TestSearchMethods(TestCase):
         self.assertEqual(self.search_helper.combinations[3], "main query)keyword2)keyword3")
         self.assertEqual(self.search_helper.combinations[4], "main query)keyword3")
         self.assertEqual(self.search_helper.combinations[5], "main query")
+
+
+class TestArticle(TestCase):
+    def test_article_to_json(self):
+        # given
+        pm_id = "738217"
+        title = "article title"
+        pubmed_link = "www.pubmed.com"
+        article_link = "localhost" + "/articles/" + pm_id
+        author_list = ["author1", "author2"]
+        article = Article(pm_id=pm_id, title=title, journal_issn="", journal_name="", abstract="", pubmed_link=pubmed_link,
+                          article_link=article_link, author_list=author_list, instutation_list=[], article_date="",
+                          top_three_keywords=[], article_type="")
+
+        # when
+        article_json = article.toJSON()
+
+        # then
+        expected_value = {"pm_id": pm_id, "title": title, "authors": author_list, "pubmed_link": pubmed_link,
+                          "article_link": article_link, "article_type": "", "article_date": ""}
+        self.assertEqual(article_json, expected_value)
